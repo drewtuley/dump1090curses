3ab053698ce0d1e4616fef9c675ad3a08459f71c
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-04-23 16:23:18 +0100

    add curses/thread tester

--- src/curs.py
+++ src/curs.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python
 
-import curses, time
+import curses
+import time
 
 
 def main(screen):
-	curses.start_color()
-	curses.init_pair(1,curses.COLOR_GREEN, curses.COLOR_BLUE)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE)
 
-	screen.refresh()
+    screen.refresh()
 
-	win = curses.newwin(10,80,5,5)
-	win.bkgd(curses.color_pair(1))
-	win.box()
-	win.addstr(2,2,'hi')
-	win.refresh()
-	x=1
-	while x<10:
-		time.sleep(1)
-		win.erase()
-		win.addstr(2,2, str(x))
-		win.refresh()
-		x=x+1
+    win = curses.newwin(10, 80, 5, 5)
+    win.bkgd(curses.color_pair(1))
+    win.box()
+    win.addstr(2, 2, 'hi')
+    win.refresh()
+    x = 1
+    while x < 10:
+        time.sleep(1)
+        win.erase()
+        win.addstr(2, 2, str(x))
+        win.refresh()
+        x = x + 1
 	
-	c = screen.getch()
+    c = screen.getch()
 
 try:
-	curses.wrapper(main)
+    curses.wrapper(main)
 except KeyboardInterrupt:
 	exit()
 
