c5a33f5463adf7466c79514f3aceadff0af42737
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-04-30 13:32:55 +0100

    something to read the saved regs with

---
+++ src/read_regs.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "andrew.tuley"
+__date__ = "$30-Apr-2015 13:27:11$"
+import shelve
+
+CALLSIGNS = 'callsigns'
+if __name__ == "__main__":
+    db = shelve.open('planes.db')
+    
+    if db.has_key(CALLSIGNS):
+        cs = db[CALLSIGNS]
+        for k in cs:
+            print (k+'='+cs[k])
