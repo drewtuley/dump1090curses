bd6f92802adfc1f7e47691f581d0973ca3cca756
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-05-06 09:02:57 +0100

    use python run scripts instead

---
+++ scripts/read-regs.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "andrew.tuley"
+__date__ = "$06-May-2015 08:49:11$"
+
+import os
+
+if __name__ == "__main__":
+    home=os.getenv('HOME')
+    if len(home) > 0:
+        dump1090=home+'/git/dump1090curses'
+        data=dump1090+'/data'
+        if not os.access(data, os.R_OK):
+            print 'Unable to access data dir:'+data
+            exit(1)
+        os.environ['REGDBNAME'] = data
+        os.execl(dump1090+'/src/read_regs.py','x')
