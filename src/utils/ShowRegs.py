c8313e6bfb1e0850051f9395adc82be43887f76a
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-05-07 09:14:20 +0100

    add utils

---
+++ src/utils/ShowRegs.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "andrew.tuley"
+__date__ = "$07-May-2015 09:11:32$"
+
+import sqlite3
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('data/sqlite_planes.db')
+    
+    crs = conn.cursor()
+    
+    crs.execute('select * from registration')
+    for row in crs.fetchall():
+        icao, reg, dt = row
+        print icao, reg, dt
+        
+    conn.close()
---
+++ src/utils/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "andrew.tuley"
+__date__ = "$07-May-2015 09:10:34$"
