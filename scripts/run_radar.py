e4fb226b60b873e83c62f9bfd94eb19a97753730
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-05-28 11:53:43 +0100

    mods to startup script

--- scripts/run_radar.py
+++ scripts/run_radar.py
@@ -14,8 +14,14 @@ if __name__ == "__main__":
     home=os.getenv('HOME')
     if len(home) > 0:
         dump1090=home+'/git/dump1090curses'
-        data=dump1090+'/data'
-        logdir=dump1090+'/log'
+        if not os.access(dump1090, os.X_OK):
+            print 'Error: Unable to access dump1090 dir'
+            exit(1)
+        
+        os.chdir(dump1090)
+        
+        data='data'
+        logdir='log'
         if not os.access(data, os.R_OK):
             print 'Warning: Unable to access data dir:'+data
             os.mkdir(data)
@@ -25,14 +31,14 @@ if __name__ == "__main__":
             
         os.environ['REGDBNAME'] = data+'/sqlite_planes.db'
         os.environ['LOGDIR'] = logdir
-        script=dump1090+'/src/radar.py'
+        script='src/radar.py'
 	lines = 23
 	with os.popen('tput lines') as fd:
             for line in fd:
 		lines = (string.rstrip(line))
 	print 'opening with '+str(lines)+' lines'
         if os.access(script, os.X_OK):
-            os.execl(dump1090+'/src/radar.py','x',lines)
+            os.execl(script,'x',lines)
         else:
             print 'Error: unable to execute '+script
             exit(1)
