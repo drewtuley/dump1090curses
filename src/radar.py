e4e5fc9b7ca3dacaeac14b0c48600f1d90ecd5ee
drewtuley <drewtuley@gmail.com>
2016-06-27 09:54:29 +0100

    reformatting

--- src/radar.py
+++ src/radar.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 ## message format from dump1090:30003 as..
-#MSG,8,111,11111,405B77,111111,2015/04/15,08:19:34.893,2015/04/15,08:19:34.886,,,,,,,,,,,,0
-#MSG,7,111,11111,405B77,111111,2015/04/15,08:19:35.154,2015/04/15,08:19:35.148,,24000,,,,,,,,,,0
-#MSG,6,111,11111,405B77,111111,2015/04/15,08:19:35.255,2015/04/15,08:19:35.218,BEE8WQ  ,,,,,,,7634,0,0,0,0
-#MSG,5,111,11111,40649F,111111,2015/04/15,08:19:35.612,2015/04/15,08:19:35.606,,25700,,,,,,,0,,0,0
-#MSG,7,111,11111,40649F,111111,2015/04/15,08:19:35.880,2015/04/15,08:19:35.869,,25725,,,,,,,,,,0
+# MSG,8,111,11111,405B77,111111,2015/04/15,08:19:34.893,2015/04/15,08:19:34.886,,,,,,,,,,,,0
+# MSG,7,111,11111,405B77,111111,2015/04/15,08:19:35.154,2015/04/15,08:19:35.148,,24000,,,,,,,,,,0
+# MSG,6,111,11111,405B77,111111,2015/04/15,08:19:35.255,2015/04/15,08:19:35.218,BEE8WQ  ,,,,,,,7634,0,0,0,0
+# MSG,5,111,11111,40649F,111111,2015/04/15,08:19:35.612,2015/04/15,08:19:35.606,,25700,,,,,,,0,,0,0
+# MSG,7,111,11111,40649F,111111,2015/04/15,08:19:35.880,2015/04/15,08:19:35.869,,25725,,,,,,,,,,0
 
 
 import curses
@@ -31,50 +31,52 @@ RADAR24URL = 'https://api.flightradar24.com/common/v1/search.json?fetchBy=reg&qu
 cols = 155
 rows = 28
 
+
 def removeplanes():
     """ Remove any plane with eventdate older than 30s """
 
     for id in planes:
         plane = planes[id]
-        if (datetime.now()-plane.eventdate).total_seconds() > 30 and plane.active:
-            plane.active = False	
-            logging.debug('add plane id:'+id+' to inactive queue')
+        if (datetime.now() - plane.eventdate).total_seconds() > 30 and plane.active:
+            plane.active = False
+            logging.debug('add plane id:' + id + ' to inactive queue')
             inactive_queue.append(id)
+
 
 def getplanes(lock, run, config):
     c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c_socket.connect(('localhost', int(config.get('dump1090','port'))))
-    c_socket.settimeout(float(config.get('dump1090','timeout')))
+    c_socket.connect(('localhost', int(config.get('dump1090', 'port'))))
+    c_socket.settimeout(float(config.get('dump1090', 'timeout')))
 
     while run['run']:
         try:
-			lines = c_socket.recv(4096)
-			for line in lines.strip().split('\n'):
-				logging.debug('got line:'+line.strip())
-				parts = [x.strip() for x in line.split(',')]
-				if parts[0] == 'MSG' and parts[4] != '000000':
-					id = parts[4]
-					lock.acquire()
-					if id in planes:
-						plane = planes[id]
-					else:
-						plane = Plane(id, datetime.now())
-						registration_queue.append(id)
-						run['session_count'] += 1
-						planes[id] = plane
-						
-					plane.update(parts)
-					removeplanes()
-					lock.release()
-				elif parts[0] == 'STA':
-					id = parts[4]
-					status = parts[10]
-					if status == 'RM':
-					    plane = planes[id]
-					    plane.active = False
-					    logging.debug('set id: '+id+' inactive due to dump1090 remove')
-                        inactive_queue.append(id)
-					    
+            lines = c_socket.recv(4096)
+            for line in lines.strip().split('\n'):
+                logging.debug('got line:' + line.strip())
+                parts = [x.strip() for x in line.split(',')]
+                if parts[0] == 'MSG' and parts[4] != '000000':
+                    id = parts[4]
+                    lock.acquire()
+                    if id in planes:
+                        plane = planes[id]
+                    else:
+                        plane = Plane(id, datetime.now())
+                        registration_queue.append(id)
+                        run['session_count'] += 1
+                        planes[id] = plane
+
+                    plane.update(parts)
+                    removeplanes()
+                    lock.release()
+                elif parts[0] == 'STA':
+                    id = parts[4]
+                    status = parts[10]
+                    if status == 'RM':
+                        plane = planes[id]
+                        plane.active = False
+                        logging.debug('set id: ' + id + ' inactive due to dump1090 remove')
+            inactive_queue.append(id)
+
         except:
             pass
     logging.debug('exit getplanes')
@@ -87,16 +89,16 @@ def showplanes(win, lock, run):
         row = 2
         win.erase()
         Plane.showheader(win)
-        #lock.acquire()
-        cached=0
-        
+        # lock.acquire()
+        cached = 0
+
         for id in sorted(planes, key=planes.__getitem__):
             if planes[id].active:
                 if row < rows - 1:
                     planes[id].showincurses(win, row)
                     if planes[id].from_antenna > max_distance:
                         max_distance = planes[id].from_antenna
-                        
+
                     if planes[id].registration[-1:] == '*':
                         cached += 1
                     row += 1
@@ -108,77 +110,86 @@ def showplanes(win, lock, run):
         for id in planes:
             if planes[id].active:
                 current += 1
-                
+
         if current > run['session_max']:
-            run['session_max'] = len(planes)  
-           
-        try: 
-            coverage = cached*100/current    
+            run['session_max'] = len(planes)
+
+        try:
+            coverage = cached * 100 / current
         except:
             coverage = 0
 
         try:
-            win.addstr(rows-1, 1, 'Current:{}  Total (session):{}  Max (session):{}  Reg Cache:{}%  Max Distance:{:3.1f}nm'.format(str(current),str(run['session_count']),str(run['session_max']),str(int(coverage)),max_distance))
-            win.addstr(rows-1, cols-5-len(now), now)
+            win.addstr(rows - 1, 1,
+                       'Current:{}  Total (session):{}  Max (session):{}  Reg Cache:{}%  Max Distance:{:3.1f}nm'.format(
+                           str(current), str(run['session_count']), str(run['session_max']), str(int(coverage)),
+                           max_distance))
+            win.addstr(rows - 1, cols - 5 - len(now), now)
         except:
             pass
-        
-        #lock.release()
+
+        # lock.release()
         win.refresh()
 
-	
+
 def open_database(config):
-    dbname = config.get('directories','data')+'/'+config.get('database','dbname')
-    logging.info('Opening db '+dbname)
+    dbname = config.get('directories', 'data') + '/' + config.get('database', 'dbname')
+    logging.info('Opening db ' + dbname)
     return sqlite3.connect(dbname)
-     
+
 
 def close_database(conn):
     logging.info('Closing db')
-    sql = 'update observation set endtime="'+str(datetime.now())+'" where endtime is null'
+    sql = 'update observation set endtime="' + str(datetime.now()) + '" where endtime is null'
     conn.execute(sql)
     conn.commit()
     conn.close()
 
+
 def update_registration(reg, id, conn):
-    sql = 'insert into registration select "'+id+'", "'+reg+'","'+str(datetime.now())+'"'
-    logging.debug('Update db with:'+sql)
+    sql = 'insert into registration select "' + id + '", "' + reg + '","' + str(datetime.now()) + '"'
+    logging.debug('Update db with:' + sql)
     upd = conn.execute(sql)
     conn.commit()
-    logging.debug('update result='+str(upd.description))
-    
+    logging.debug('update result=' + str(upd.description))
+
+
 def log_observation_start(id, conn, curr_instance):
-    sql = 'insert into observation select "'+str(curr_instance+1)+'","'+id+'","'+str(datetime.now())+'",null '
-    logging.debug('adding observation with SQL:'+sql)
+    sql = 'insert into observation select "' + str(curr_instance + 1) + '","' + id + '","' + str(
+        datetime.now()) + '",null '
+    logging.debug('adding observation with SQL:' + sql)
     conn.execute(sql)
     conn.commit()
 
-    #sql = 'select max(instance) from observation where icao_code ="'+id+'"'
-    #crs = conn.cursor()
-    #crs.execute(sql)
-    #instance = 0
-    #for row in crs.fetchall():
-     #   instance, = row
+    # sql = 'select max(instance) from observation where icao_code ="'+id+'"'
+    # crs = conn.cursor()
+    # crs.execute(sql)
+    # instance = 0
+    # for row in crs.fetchall():
+    #   instance, = row
 
-    #logging.debug('ICAO '+id+' shows observation instance of '+str(instance))   
-    return curr_instance+1
-    
+    # logging.debug('ICAO '+id+' shows observation instance of '+str(instance))
+    return curr_instance + 1
+
+
 def log_observation_end(id, instance, conn):
-    sql = 'update observation set endtime = "'+str(datetime.now())+'" where icao_code = "'+id+'" and endtime is null and instance ='+str(instance)
-    logging.debug('ending observation with SQL:'+sql)
+    sql = 'update observation set endtime = "' + str(
+        datetime.now()) + '" where icao_code = "' + id + '" and endtime is null and instance =' + str(instance)
+    logging.debug('ending observation with SQL:' + sql)
     conn.execute(sql)
     conn.commit()
-        
+
+
 def get_registration(id, conn, reg_cache, config):
     reg = ''
     instance = 0
-    
+
     if id in reg_cache.keys():
-        reg = reg_cache[id][0]+'*'
+        reg = reg_cache[id][0] + '*'
         instance = reg_cache[id][1]
-        logging.info('Registration {} instance {} in cache'.format(reg[:-1],instance))
-    else:    
-        sql = 'select registration from registration where icao_code = "'+id+'"'
+        logging.info('Registration {} instance {} in cache'.format(reg[:-1], instance))
+    else:
+        sql = 'select registration from registration where icao_code = "' + id + '"'
         cursor = conn.cursor()
         cursor.execute(sql)
 
@@ -186,76 +197,80 @@ def get_registration(id, conn, reg_cache, config):
             registration = row
             if len(registration) > 0:
                 reg_cache[id] = registration[0]
-                instance=0
-                reg=registration[0]+'*'
-                logging.info('Reg '+registration[0]+' in DB')
+                instance = 0
+                reg = registration[0] + '*'
+                logging.info('Reg ' + registration[0] + ' in DB')
         if len(reg) == 0:
             # no reg in db, so try FR24 
-           reg = get_registration_from_fr24(id, config)
-           if len(reg)>0 and reg != 'x':
-               reg_cache[id] = reg
-               instance=0
-               update_registration(reg, id, conn)
+            reg = get_registration_from_fr24(id, config)
+            if len(reg) > 0 and reg != 'x':
+                reg_cache[id] = reg
+                instance = 0
+                update_registration(reg, id, conn)
 
-    return (reg,instance)
-    
+    return (reg, instance)
+
+
 def get_registration_from_fr24(id, config):
-        """ 
-        Not sure how long radar24 will keep this REST endpoint exposed 
-        But might as well use it while we can
-        """
-        url = config.get('fr24','api')
-        geturl = url.format(str(id))
-        logging.debug('lookup '+str(id)+' on FR24 via:'+geturl)
-        try:
-            response = requests.get(geturl)
-            logging.debug(response.json()['results'])
-            if response.status_code == 200:
-                try:
-					return response.json()['results'][0]['id']
-                except KeyError:
-                    return ''
-            else:
+    """
+    Not sure how long radar24 will keep this REST endpoint exposed
+    But might as well use it while we can
+    """
+    url = config.get('fr24', 'api')
+    geturl = url.format(str(id))
+    logging.debug('lookup ' + str(id) + ' on FR24 via:' + geturl)
+    try:
+        response = requests.get(geturl)
+        logging.debug(response.json()['results'])
+        if response.status_code == 200:
+            try:
+                return response.json()['results'][0]['id']
+            except KeyError:
                 return ''
-        except:
-            return 'x'
+        else:
+            return ''
+    except:
+        return 'x'
+
 
 def get_locations(conn):
     """ Load my recognizeable locations from location table in db """
-    
+
     locations = {}
-    crsr=conn.cursor()
+    crsr = conn.cursor()
     crsr.execute('select * from location')
     for row in crsr.fetchall():
         place, lat, long, = row
-        locations[place]=(lat,long)
-        
+        locations[place] = (lat, long)
+
     return locations
+
 
 def warm_reg_cache(conn):
     crsr = conn.cursor()
-    crsr.execute('select r.icao_code, r.registration, max(o.instance) '\
-        'from registration r, observation o where r.icao_code = o.icao_code group by r.icao_code, r.registration')
+    crsr.execute('select r.icao_code, r.registration, max(o.instance) ' \
+                 'from registration r, observation o where r.icao_code = o.icao_code group by r.icao_code, r.registration')
     cache = {}
     for row in crsr.fetchall():
         icao, reg, instance, = row
-        cache[icao] = (reg,instance)
-        
-    logging.info('Loaded {} registrations into cache'.format(len(cache)))    
-    return cache    
+        cache[icao] = (reg, instance)
+
+    logging.info('Loaded {} registrations into cache'.format(len(cache)))
+    return cache
+
 
 def get_registrations(lock, runstate, config):
     conn = open_database(config)
     locations_from_db = get_locations(conn)
-    if len(locations_from_db) >0:
+    if len(locations_from_db) > 0:
         Plane.locations = locations_from_db
-    
-    reg_cache = warm_reg_cache(conn)    
-        
+
+    reg_cache = warm_reg_cache(conn)
+
     while runstate['run']:
         regs = copy.copy(registration_queue)
         for id in regs:
-            reg,curr_instance = get_registration(id, conn, reg_cache, config)
+            reg, curr_instance = get_registration(id, conn, reg_cache, config)
             instance = log_observation_start(id, conn, curr_instance)
             reg_cache[id] = (reg, instance)
             lock.acquire()
@@ -263,64 +278,67 @@ def get_registrations(lock, runstate, config):
             planes[id].observe_instance = instance
             registration_queue.remove(id)
             lock.release()
-            
+
         inactives = copy.copy(inactive_queue)
         for id in inactives:
             try:
                 instance = planes[id].observe_instance
                 log_observation_end(id, instance, conn)
             except:
-                logging.debug('unable to log observation end for id '+str(id))
+                logging.debug('unable to log observation end for id ' + str(id))
             lock.acquire()
             inactive_queue.remove(id)
             lock.release()
-            
+
         time.sleep(.0500)
     close_database(conn)
     logging.debug('exit get_registrations')
-    
+
+
 def main(screen):
     urllib3.contrib.pyopenssl.inject_into_urllib3()
     config = ConfigParser.SafeConfigParser()
     config.read('dump1090curses.props')
-    dt=str(datetime.now())[:10]
-    
-    logging.basicConfig(format='%(asctime)s %(message)s', filename=config.get('directories','log')+'/'+config.get('logging','logname')+dt+'.log', level=logging.DEBUG)
+    dt = str(datetime.now())[:10]
+
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                        filename=config.get('directories', 'log') + '/' + config.get('logging',
+                                                                                     'logname') + dt + '.log',
+                        level=logging.DEBUG)
     logging.captureWarnings(True)
     curses.start_color()
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
- 
+
     screen.refresh()
 
     win = curses.newwin(rows, cols, 1, 1)
     win.bkgd(curses.color_pair(1))
     win.box()
 
-    runstate = {'run':True, 'session_count':0, 'session_max':0}
+    runstate = {'run': True, 'session_count': 0, 'session_max': 0}
     lock = thread.allocate_lock()
-    get = threading.Thread(target=getplanes, args=(lock, runstate, config ))
-    show = threading.Thread(target=showplanes, args=(win, lock, runstate ))
-    registration = threading.Thread(target=get_registrations, args=(lock, runstate, config ))
-    
+    get = threading.Thread(target=getplanes, args=(lock, runstate, config))
+    show = threading.Thread(target=showplanes, args=(win, lock, runstate))
+    registration = threading.Thread(target=get_registrations, args=(lock, runstate, config))
+
     get.start()
     show.start()
     registration.start()
-    
+
     while runstate['run']:
         ch = screen.getch()
         if ch == 113:
             runstate['run'] = False
             logging.debug('kill requested by user')
-        
+
     time.sleep(2)
+
 
 # usage: radar.py [screen rows]
 if len(sys.argv) > 1:
-   rows=int(sys.argv[1])-4
+    rows = int(sys.argv[1]) - 4
 try:
     curses.wrapper(main)
 except:
     exit()
-
-	
--- src/scraperegs.py
+++ src/scraperegs.py
@@ -10,82 +10,88 @@ import sys
 
 RADAR24URL = 'https://api.flightradar24.com/common/v1/search.json?fetchBy=reg&query='
 
+
 def open_database(config):
-    dbname = config.get('directories','data')+'/'+config.get('database','dbname')
-    logging.info('Opening db '+dbname)
+    dbname = config.get('directories', 'data') + '/' + config.get('database', 'dbname')
+    logging.info('Opening db ' + dbname)
     return sqlite3.connect(dbname)
-     
+
 
 def close_database(conn):
     logging.info('Closing db')
-    sql = 'update observation set endtime="'+str(datetime.now())+'" where endtime is null'
+    sql = 'update observation set endtime="' + str(datetime.now()) + '" where endtime is null'
     conn.execute(sql)
     conn.commit()
     conn.close()
 
+
 def update_registration(reg, id, conn):
-    sql = 'insert into registration select "'+id.upper()+'", "'+reg.upper()+'","'+str(datetime.now())+'"'
-    logging.debug('Update db with:'+sql)
+    sql = 'insert into registration select "' + id.upper() + '", "' + reg.upper() + '","' + str(datetime.now()) + '"'
+    logging.debug('Update db with:' + sql)
     upd = conn.execute(sql)
     conn.commit()
-    logging.debug('update result='+str(upd.description))
+    logging.debug('update result=' + str(upd.description))
 
 
 def load_ids(conn):
-	ids = []
-	sql = 'select icao_code from registration'
-	logging.debug('select loaded ids with:'+sql)
-	cursor = conn.cursor()
-	cursor.execute(sql)
+    ids = []
+    sql = 'select icao_code from registration'
+    logging.debug('select loaded ids with:' + sql)
+    cursor = conn.cursor()
+    cursor.execute(sql)
 
-	for id in cursor.fetchall():
-		ids.append(id[0].upper())
-    
-	return ids
+    for id in cursor.fetchall():
+        ids.append(id[0].upper())
+
+    return ids
 
 
 if len(sys.argv) > 1:
-	start = end = 0
-	try:
-		start= int(sys.argv[1], 16)
-		end = int(sys.argv[2], 16)
-	except ValueError:
-		pass
-	if start <> 0 and end <> 0:
-		print ('start:{} end:{}'.format(start, end))
+    start = end = 0
+    try:
+        start = int(sys.argv[1], 16)
+        end = int(sys.argv[2], 16)
+    except ValueError:
+        pass
+    if start != 0 and end != 0:
+        print('start:{} end:{}'.format(start, end))
 
-		urllib3.contrib.pyopenssl.inject_into_urllib3()
-		config = ConfigParser.SafeConfigParser()
-		config.read('dump1090curses.props')
-		dt=str(datetime.now())[:10]
+        urllib3.contrib.pyopenssl.inject_into_urllib3()
+        config = ConfigParser.SafeConfigParser()
+        config.read('dump1090curses.props')
+        dt = str(datetime.now())[:10]
 
-		logging.basicConfig(format='%(asctime)s %(message)s', filename=config.get('directories','log')+'/'+config.get('logging','scrapelog')+dt+'.log', level=logging.DEBUG)
-		logging.captureWarnings(True)
+        logging.basicConfig(format='%(asctime)s %(message)s',
+                            filename=config.get('directories', 'log') + '/' + config.get('logging',
+                                                                                         'scrapelog') + dt + '.log',
+                            level=logging.DEBUG)
+        logging.captureWarnings(True)
 
-		conn = open_database(config)
-		ids = load_ids(conn)
-		logging.debug('loaded {} cached ids'.format(len(ids)))
+        conn = open_database(config)
+        ids = load_ids(conn)
+        logging.debug('loaded {} cached ids'.format(len(ids)))
 
-		icao = start
-		while icao <= end:
-			print('start at {:x}'.format(icao))
-			id='{:X}'.format(icao)[:-1]
-			geturl = RADAR24URL + str(id)+'*'
-			logging.debug('lookup '+str(id)+' on FR24 via:'+geturl)
-			try:
-				response = requests.get(geturl)
-				#print (response.json()['result'])
-				if response.status_code == 200:
-					try:
-						for data in response.json()['result']['response']['aircraft']['data']:
-							reg = data['registration']
-							hex = data['hex']
-							if hex.upper() not in ids:
-								print '{0},{1}'.format(hex, reg)
-								update_registration(reg, hex, conn)
-					except KeyError:
-						reg=''
-			except:
-				reg=''
-			icao += 16 
-		close_database(conn)
+        icao = start
+        while icao <= end:
+            print('start at {:x}'.format(icao))
+            id = '{:X}'.format(icao)[:-1]
+            geturl = RADAR24URL + str(id) + '*'
+            logging.debug('lookup ' + str(id) + ' on FR24 via:' + geturl)
+            try:
+                response = requests.get(geturl)
+                # print (response.json()['result'])
+                if response.status_code == 200:
+                    try:
+                        for data in response.json()['result']['response']['aircraft']['data']:
+                            reg = data['registration']
+                            hex = data['hex']
+                            if hex.upper() not in ids:
+                                print
+                                '{0},{1}'.format(hex, reg)
+                                update_registration(reg, hex, conn)
+                    except KeyError:
+                        reg = ''
+            except:
+                reg = ''
+            icao += 16
+        close_database(conn)
