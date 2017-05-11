24ade283557f869e2a9b92a390b3add0d81dac85
drewtuley <drewtuley@gmail.com>
2017-05-11 13:55:01 +0100

    update live_fiddlin on PyCharm

--- src/radar.py
+++ src/radar.py
@@ -26,7 +26,6 @@ import urllib3.contrib.pyopenssl
 planes = {}
 registration_queue = []
 inactive_queue = []
-RADAR24URL = 'https://api.flightradar24.com/common/v1/search.json?fetchBy=reg&query='
 
 cols = 155
 rows = 28
@@ -52,30 +51,32 @@ def getplanes(lock, run, config):
         try:
             lines = c_socket.recv(4096)
             for line in lines.strip().split('\n'):
-				if len(line.strip()) > 0:
-					logging.debug('got line:' + line.strip())
-					parts = [x.strip() for x in line.split(',')]
-					if parts[0] == 'MSG' and parts[4] != '000000':
-						id = parts[4]
-						lock.acquire()
-						if id in planes:
-							plane = planes[id]
-						else:
-							plane = Plane(id, datetime.now())
-							registration_queue.append(id)
-							run['session_count'] += 1
-							planes[id] = plane
+                if len(line.strip()) > 0:
+                    logging.debug('got line:' + line.strip())
+                    parts = [x.strip() for x in line.split(',')]
+                    if parts[0] == 'MSG' and parts[4] != '000000':
+                        id = parts[4]
+                        lock.acquire()
+                        if id in planes:
+                            plane = planes[id]
+                        else:
+                            plane = Plane(id, datetime.now())
+                            registration_queue.append(id)
+                            run['session_count'] += 1
+                            planes[id] = plane
 
-						plane.update(parts)
-						removeplanes()
-						lock.release()
-					elif parts[0] == 'STA':
-						id = parts[4]
-						status = parts[10]
-						if status == 'RM':
-							plane = planes[id]
-							plane.active = False
-							logging.debug('set id: ' + id + ' inactive due to dump1090 remove')
+                        plane.update(parts)
+                        removeplanes()
+                        lock.release()
+                    elif parts[0] == 'MSG' and parts[4] == '000000' and int(parts[1]) == 7:
+                        removeplanes()
+                    elif parts[0] == 'STA':
+                        id = parts[4]
+                        status = parts[10]
+                        if status == 'RM':
+                            plane = planes[id]
+                            plane.active = False
+                            logging.debug('set id: ' + id + ' inactive due to dump1090 remove')
             inactive_queue.append(id)
 
         except:
--- src/socketradar.py
+++ src/socketradar.py
@@ -4,17 +4,16 @@ import socket
 import signal
 import sys
 
-
 c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c_socket.connect(('localhost', 30003))
 c_socket.settimeout(1.0)
 
 signal.signal(signal.SIGINT, signal.default_int_handler)
 while True:
-	try:
-		buf = c_socket.recv(4096)
-		print (buf.strip())
-	except KeyboardInterrupt:
-		exit(1)
-	except:
-		pass
+    try:
+        buf = c_socket.recv(4096)
+        print(buf.strip())
+    except KeyboardInterrupt:
+        exit(1)
+    except:
+        pass
