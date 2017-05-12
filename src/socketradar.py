42caf135a7e8a55e394982b34ca5c1d6563542ec
Andrew Tuley <drewtuley@gmail.com>
2017-05-12 07:45:34 +0100

    add timestamp to raw data readout

--- src/socketradar.py
+++ src/socketradar.py
@@ -2,18 +2,22 @@
 
 import socket
 import signal
-import sys
+import time
+
+signal.signal(signal.SIGINT, signal.default_int_handler)
 
 c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c_socket.connect(('localhost', 30003))
 c_socket.settimeout(1.0)
 
-signal.signal(signal.SIGINT, signal.default_int_handler)
 while True:
     try:
         buf = c_socket.recv(4096)
-        print(buf.strip())
+        if len(buf) < 1:
+            break
+        print('{0} {1}'.format(time.time(), buf.strip()))
     except KeyboardInterrupt:
         exit(1)
     except:
         pass
+c_socket.close()
