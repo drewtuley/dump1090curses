4612f3a20068bdb308b63ef2f1611e6261e98397
Andrew Tuley <drewtuley@gmail.com>
2017-05-15 19:07:34 +0100

    print individual lines

--- src/socketradar.py
+++ src/socketradar.py
@@ -7,40 +7,40 @@ import sys
 from datetime import datetime
 
 
-if len(sys.argv) > 1:
-    o_file = sys.argv[1]
-else:
+if len(sys.argv) == 1:
     print('Usage: {0} <output file>'.format(sys.argv[0]))
     exit(1)
+else:
+    o_file = sys.argv[1]
 
-with open(o_file, 'a') as fd:
-    signal.signal(signal.SIGINT, signal.default_int_handler)
-
-    while True:
-        connected = False
-        while not connected:
-            try:
-                c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                c_socket.connect(('localhost', 30003))
-                c_socket.settimeout(1.0)
-                connected = True
-            except socket.error, ex:
-                print('{0}: Failed to connect : {1}'.format(str(datetime.now())[:19], ex))
-                time.sleep(1)
+    with open(o_file, 'a') as fd:
+        signal.signal(signal.SIGINT, signal.default_int_handler)
 
         while True:
-            try:
-                buf = c_socket.recv(4096)
-                if len(buf) < 1:
-                    print('{0}: Possible buffer underrun - close/reopen'.format(str(datetime.now())[:19]))
-                    break
-                print('{2}: Writing {0} bytes to {1}'.format(str(len(buf)), o_file, str(datetime.now())[:19]))
-                fd.writelines('{0} {1}\n'.format(time.time(), buf.strip()))
-                fd.flush()
-            except KeyboardInterrupt:
-                exit(1)
-            except socket.error ,v:
-                #print('Exception {0}'.format(v))
-                pass
-        c_socket.close()
-    fd.close()
+            connected = False
+            while not connected:
+                try:
+                    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    c_socket.connect(('localhost', 30003))
+                    c_socket.settimeout(1.0)
+                    connected = True
+                except socket.error, ex:
+                    print('{0}: Failed to connect : {1}'.format(str(datetime.now())[:19], ex))
+                    time.sleep(1)
+
+            while True:
+                try:
+                    buf = c_socket.recv(4096)
+                    if len(buf) < 1:
+                        print('{0}: Possible buffer underrun - close/reopen'.format(str(datetime.now())[:19]))
+                        break
+                    print('{2}: Writing {0} bytes to {1}'.format(str(len(buf)), o_file, str(datetime.now())[:19]))
+                    for line in buf.strip().split('\n'):
+                        fd.writelines('{0} {1}\n'.format(time.time(), line))
+                    fd.flush()
+                except KeyboardInterrupt:
+                    exit(1)
+                except socket.error ,v:
+                    #print('Exception {0}'.format(v))
+                    pass
+            c_socket.close()
