e47f8e5677d16f1f8f0a0fb7f3137a7c37f38f47
Andrew Tuley <drewtuley@gmail.com>
2017-05-15 11:08:04 +0100

    retry with 1s wait in event of connection failure

--- src/socketradar.py
+++ src/socketradar.py
@@ -17,15 +17,22 @@ with open(o_file, 'a') as fd:
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
     while True:
-        c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        c_socket.connect(('localhost', 30003))
-        c_socket.settimeout(1.0)
+        connected = False
+        while not connected:
+            try:
+                c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                c_socket.connect(('localhost', 30003))
+                c_socket.settimeout(1.0)
+                connected = True
+            except socket.error, ex:
+                print('{0}: Failed to connect : {1}'.format(str(datetime.now())[:19], ex))
+                time.sleep(1)
 
         while True:
             try:
                 buf = c_socket.recv(4096)
                 if len(buf) < 1:
-                    print('Possible buffer underrun - close/reopen')
+                    print('{0}: Possible buffer underrun - close/reopen'.format(str(datetime.now())[:19]))
                     break
                 print('{2}: Writing {0} bytes to {1}'.format(str(len(buf)), o_file, str(datetime.now())[:19]))
                 fd.writelines('{0} {1}\n'.format(time.time(), buf.strip()))
