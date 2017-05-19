b0c6d8b6082db5447a2fd9e72d535acdf5a8a8c5
Andrew Tuley <drewtuley@gmail.com>
2017-05-19 16:01:07 +0100

    add host to props and make test server overwrite timestamp with real time

--- src/test_dump_client.py
+++ src/test_dump_client.py
@@ -1,14 +1,13 @@
 import ConfigParser
 import socket
 
-TCP_IP = 'localhost'
 BUFFER_SIZE = 1024
 
 config = ConfigParser.SafeConfigParser()
 config.read('dump1090curses.props')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, int(config.get('dump1090_test_server', 'port'))))
+s.connect((config.get('dump1090_test_server','host'), int(config.get('dump1090_test_server', 'port'))))
 s.settimeout(float(config.get('dump1090_test_server', 'timeout')))
 
 while True:
--- src/test_dump_server.py
+++ src/test_dump_server.py
@@ -3,8 +3,8 @@ import socket
 import time
 import thread
 import threading
-
-TCP_IP = 'localhost'
+import sys
+from datetime import datetime
 
 
 def send_data(conn, dump_file):
@@ -13,35 +13,43 @@ def send_data(conn, dump_file):
         wait = None
         for line in fd:
             ln = line.strip()
-            parts = ln.split(' ')
+            msgparts = ln.split(',')
+            parts = msgparts[0].split(' ')
             timecode = float(parts[0])
+            msgparts[0] = parts[1]
+
 
             if last_timecode:
                 wait = (timecode - last_timecode) * 0.1
 
             last_timecode = timecode
 
-            if wait:
+            if str(msgparts[4]) != '000000' and wait:
                 time.sleep(wait)
-
-            print("send data:", parts[1:])
-            conn.send(','.join(parts[1:]))  # echo
+            # replace parts[6] & parts[7] with current time.....
+            msgparts[6] = datetime.now().strftime('%Y/%m/%d')
+            msgparts[7] = datetime.now().strftime('%H:%M:%S.%f')
+            data = ','.join(msgparts)+'\n'
+            print('send data: {}'.format(data))
+            conn.send(data)  # echo
 
         conn.close()
 
 
-config = ConfigParser.SafeConfigParser()
-config.read('dump1090curses.props')
+if len(sys.argv) > 1:
+        dump_file = sys.argv[1]
+        config = ConfigParser.SafeConfigParser()
+        config.read('dump1090curses.props')
 
-dump_file = '/Volumes/share/dump1090_20170518.txt'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, int(config.get('dump1090_test_server', 'port'))))
-s.listen(1)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((config.get('dump1090_test_server','host'), int(config.get('dump1090_test_server', 'port'))))
+        s.listen(1)
 
-while True:
-    conn, address = s.accept()
-    print('Connection address:', address)
-    server = threading.Thread(target=send_data, args=(conn, dump_file))
-    server.start()
-
+        while True:
+            conn, address = s.accept()
+            print('Connection address:', address)
+            server = threading.Thread(target=send_data, args=(conn, dump_file))
+            server.start()
+else:
+    print('Usage: {} <dump data file>'.format(sys.argv[0]))
