6b9797b9b3c47c37c3e0a39f7b624e0a3945e007
Andrew Tuley <drewtuley@gmail.com>
2017-09-15 08:03:04 +0100

    sanitise db inputs (a bit)

--- src/add_plane.py
+++ src/add_plane.py
@@ -17,11 +17,11 @@ if __name__ == '__main__':
         for arg in sys.argv[1:]:
             p = arg.strip().split('=')
             if len(p) == 2 and p[0] in valid_keys:
-                keys[p[0]] = p[1]
+                keys[p[0]] = p[1].replace('"','').replace('\'','')
     keys['dt'] = str(datetime.now())
 
     with sqlite3.connect(db_filename) as conn:
-        sql = 'insert into registration select "{icao}","{reg}","{dt}","{type}";'.format(**keys)
-        print(sql)
+        sql = 'insert into registration select "{icao}","{reg}","{dt}","{type}" where not exists (select 1 from registration where icao_code="{icao}");'.format(**keys)
+        #print(sql)
         conn.execute(sql)
         conn.commit()
