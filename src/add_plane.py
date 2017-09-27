a129e2e01da5d0ccbf3476657fd466eaeb9f097b
Andrew Tuley <drewtuley@gmail.com>
2017-09-27 07:50:28 +0100

    add some validation on args

--- src/add_plane.py
+++ src/add_plane.py
@@ -17,8 +17,16 @@ if __name__ == '__main__':
         for arg in sys.argv[1:]:
             p = arg.strip().split('=')
             if len(p) == 2 and p[0] in valid_keys:
-                keys[p[0]] = p[1].replace('"','').replace('\'','')
+                keys[p[0]] = p[1].replace('"','').replace('\'','').upper()
     keys['dt'] = str(datetime.now())
+    try:
+        icao_hex = int(keys['icao'], 16)
+        if icao_hex >= 2**24 or icao_hex < 0:
+            print('Invalid value for ICAO code {}'.format(keys['icao']))
+            exit(1)
+    except ValueError:
+        print('Invalid value for ICAO code {}'.format(keys['icao']))
+        exit(1)
 
     with sqlite3.connect(db_filename) as conn:
         sql = 'insert into registration select "{icao}","{reg}","{dt}","{type}" where not exists (select 1 from registration where icao_code="{icao}");'.format(**keys)
