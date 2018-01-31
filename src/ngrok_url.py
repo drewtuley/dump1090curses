f856399bb44f04e1750d62a89e69a362f4974dfa
Andrew Tuley <drewtuley@gmail.com>
2018-01-31 13:57:03 +0000

    add ngrok api client to get public_url

---
+++ src/ngrok_url.py
@@ -0,0 +1,14 @@
+import requests
+
+url = 'na'
+r = requests.get('http://localhost:4040/api/tunnels')
+if r.status_code == 200:
+    if 'tunnels' in r.json():
+        tunnels = r.json()['tunnels']
+        for t in tunnels:
+            if 'public_url' in t:
+                url = t['public_url']
+
+print(url)
+
+
