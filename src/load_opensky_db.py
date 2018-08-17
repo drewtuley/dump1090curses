bfec1225f14c4de19d70383d1e28b6b35d80e6db
Andrew Tuley <drewtuley@gmail.com>
2018-08-17 15:00:54 +0100

    add new Aircraft from opensky

--- src/load_opensky_db.py
+++ src/load_opensky_db.py
@@ -8,22 +8,22 @@ config.read('dump1090curses.props')
 
 dt = str(datetime.now())[:10]
 logging.basicConfig(format='%(asctime)s %(message)s',
-                    filename=config.get('directories', 'log') + '/dbload' + dt + '.log',
+                    filename=config.get('directories', 'log') + '/opensky_load' + dt + '.log',
                     level=logging.DEBUG)
 logging.captureWarnings(True)
 
 db_filename = config.get('directories', 'data') + '/' + config.get('database', 'dbname')
-data_file = config.get('directories', 'data') + '/aircraft_db.csv'
+data_file = config.get('directories', 'data') + '/aircraftDatabase.csv'
 
 with open(data_file) as fd:
     with sqlite3.connect(db_filename) as conn:
         for l in fd:
-            p = l.strip().split(',')
-            if len(p) >= 3:
+            p = l.strip().replace('"','').split(',')
+            if len(p) >= 6:
                 hex_code = p[0].upper()
                 reg = p[1].upper()
-                icao_type = p[2].upper()
-                if hex_code != 'ICAO' and reg != '00000000' and icao_type != '0000':
+                icao_type = p[5].upper()
+                if hex_code != 'ICAO24' and reg != '' and icao_type != '':
                     sql = 'insert into registration select "{icao}","{reg}","{equip}","{dt}" where not exists (select * from registration where icao_code="{icao}");' \
                         .format(icao=hex_code, reg=reg, dt=str(datetime.now()), equip=icao_type)
                     logging.debug(sql)
