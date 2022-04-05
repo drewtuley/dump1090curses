e963e120d0ea33872b2e4b706b7c4a50768e2394
Andrew Tuley <drewtuley@gmail.com>
2022-04-05 17:29:29 +0100

    add argparse options

--- src/read1090.py
+++ src/read1090.py
@@ -2,18 +2,24 @@
 
 import socket
 import time
-import sys
+import argparse
 
-port = 30106
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-c_socket.connect(('localhost',port));
-while 1:
-	time.sleep(5);
-	data=c_socket.recv(2048)
-	if ( data == 'q' or data == 'Q'):
-		c_socket.close();
-		break;
-	else:
-		print data
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='read data from a port')
+    parser.add_argument('--host',help='hostname', default='localhost')
+    parser.add_argument('--port',help='port', default=30106)
+    args=parser.parse_args()
+
+    host = args.host
+    port = int(args.port)
+
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    c_socket.connect((host,port));
+    while 1:
+        time.sleep(5);
+        data=c_socket.recv(2048)
+        if ( data == 'q' or data == 'Q'):
+            c_socket.close();
+            break;
+        else:
+            print (data.decode('utf-8'))
