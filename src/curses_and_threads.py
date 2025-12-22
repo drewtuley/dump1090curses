17c1b1a92b0e71e997de92a04aa974b8c613e203
drewtuley <drewtuley@gmail.com>
2025-12-22 11:17:48 +0000

    some more sophisticated bouncing (#21)
    Co-authored-by: nhsd-andrew-tuley <andrew.tuley1@nhs.net>

--- src/curses_and_threads.py
+++ src/curses_and_threads.py
@@ -1,13 +1,14 @@
 import curses
 import threading
 import time
+from importlib.resources import open_text
 
 
 def worker(w, msg, run, lock):
     val = 1
     while run["run"]:
         with lock:
-            w.addstr(1, 1, msg + " " + str(val))
+            w.addstr(1, 1, "{0:} {1:>3s}".format(msg, str(val)))
             w.refresh()
         val += 1
         if val > 999:
@@ -16,56 +17,90 @@ def worker(w, msg, run, lock):
 
 
 def runner(w, run, lock):
-    x = 1
-    y = 1
+    x, y, dx, dy = (1, 1, 1, 1)
     my, mx = w.getmaxyx()
     while run["run"]:
         with lock:
-            w.addch(y, x, curses.ACS_RARROW)
+            w.addch(y, x, curses.ACS_RARROW if dx == 1 else curses.ACS_LARROW)
             w.refresh()
         time.sleep(0.1)
         with lock:
             w.addstr(y, x, " ")
-        x += 1
-        if x > mx - 2:
-            x = 1
-            y += 1
-            if y > my - 2:
-                y = 1
+        x += dx
+        if x > mx - 2 or x < 1:
+            x -= dx
+            dx = -dx
+            y += dy
+            if y > my - 2 or y < 1:
+                y -= dy
+                dy = -dy
 
 
-def move(x, y, dx, dy, mx, my):
-    x += dx
-    if not 2 <= x <= mx - 3:
-        dx *= -1
-    y += dy
-    if not 2 <= y <= my - 3:
-        dy *= -1
-
-    return x, y, dx, dy
+def draw_wall(w, wall, *walls):
+    for y, x in wall[0]:
+        w.addch(y, x, wall[1])
+    for other_wall, other_line in walls:
+        for y, x in other_wall:
+            w.addch(y, x, other_line)
 
 
 def pong(w, directions, run, lock):
-    x = 1
-    y = 1
-    dx = 1
-    dy = 1
     my, mx = w.getmaxyx()
-    w.addstr(1, 1, f"my:{my},mx:{mx}")
+    obstacles = set()
+
+    v_wall1 = set((i, 5) for i in range(4, 12))
+    v_wall2 = set((i, mx - 6) for i in range(4, 12))
+    h_wall1 = set((int(my / 2), i) for i in range(12, mx - 10))
+    h_wall2 = set((int(my / 2) + 4, i) for i in range(10, mx - 8))
+    top_wall = set((0, i) for i in range(0, mx))
+    bottom_wall = set((my - 1, i) for i in range(0, mx))
+    left_wall = set((i, 0) for i in range(1, my))
+    right_wall = set((i, mx - 1) for i in range(1, my))
+    obstacles.update(top_wall)
+    obstacles.update(bottom_wall)
+    obstacles.update(left_wall)
+    obstacles.update(right_wall)
+    obstacles.update(v_wall1)
+    obstacles.update(v_wall2)
+    obstacles.update(h_wall1)
+    obstacles.update(h_wall2)
+
+    x, y, dx, dy = (1, 1, 1, 1)
+
+    # w.addstr(1, 1, f"my:{my},mx:{mx}")
     while run["run"]:
+        new_y, new_x = (y + dy, x + dx)
+        if (new_y, new_x) in obstacles:
+            if (
+                (new_y, new_x) in top_wall
+                or (new_y, new_x) in bottom_wall
+                or (new_y, new_x) in h_wall1
+                or (new_y, new_x) in h_wall2
+            ):
+                dy = -dy
+            elif (
+                (new_y, new_x) in left_wall
+                or (new_y, new_x) in right_wall
+                or (new_y, new_x) in v_wall1
+                or (new_y, new_x) in v_wall2
+            ):
+                dx = -dx
+        else:
+            y, x = new_y, new_x
         ch = directions[(dx, dy)]
         with lock:
             w.addch(y, x, ch)
+            draw_wall(
+                w,
+                (v_wall1, curses.ACS_VLINE),
+                (v_wall2, curses.ACS_VLINE),
+                (h_wall1, curses.ACS_HLINE),
+                (h_wall2, curses.ACS_HLINE),
+            )
             w.refresh()
         time.sleep(0.1)
-        with lock:
-            w.addstr(y, x, " ")
-        (
-            x,
-            y,
-            dx,
-            dy,
-        ) = move(x, y, dx, dy, mx, my)
+        # with lock:
+        #     w.addstr(y, x, " ")
 
 
 def main(screen):
@@ -90,7 +125,7 @@ def main(screen):
         (10, 10, 1, None),
         (10, 10, 2, None),
         (10, 25, 4, None),
-        (15, 28, 3, None),
+        (16, 28, 3, None),
     ]
     windows = []
     for height, width, color, label in window_specs:
